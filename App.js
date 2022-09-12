23c5bb8d5e7bc48c688929986250cd701f230d33
Vriendl00s <99824868+Vriendl00s@users.noreply.github.com>
2022-09-12 17:09:31 +0200

    Add storage & modal & share

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Alert, Modal, Pressable, View, Button, Share } from "react-native";
 import { StatusBar } from 'expo-status-bar';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faBackspace, faTurnDown } from '@fortawesome/free-solid-svg-icons';
@@ -9,7 +10,7 @@ import * as utils from './utils';
 
 import { TileContainer, TileText, Tile, TileRow } from './styles/tiles';
 import { KeyboardContainer, KeyboardKey, KeyboardKeyText, KeyboardRow } from './styles/keyboard';
-import { Container } from './styles/home';
+import { Container, ModalContainer, ModalView, ModalText } from './styles/home';
 
 export default function App() {
     const [ guesses, setGuesses ] = useState({ ...config.newGame });
@@ -24,17 +25,40 @@ export default function App() {
     const [ letterIndex, setLetterIndex ] = useState(0);
     const [ round, setRound ] = useState(0);
     const [ todaysWord, setTodaysWord ] = useState('');
+    const [ modalVisible, setModalVisible ] = useState(false);
 
     useEffect(() => {
         dataManager.store('woordol_words', JSON.stringify(wordList));
         checkTodaysWord();
-    });
+    }, []);
+
+    useEffect(() => {
+        setData();
+    }, []);
 
     const checkTodaysWord = async () => {
         const savedWordList = JSON.parse(await dataManager.get('woordol_words'));
         const today = new Date().toISOString().slice(0, 10);
         const word = savedWordList.filter(w => w.use_on === today);
         if (word) setTodaysWord(word[0].word);
+    }
+
+    const setData = async () => {
+        const today = new Date().toISOString().slice(0, 10);
+        const savedGuesses = JSON.parse(await dataManager.get(`woordol_guesses_${today}`));
+        if (savedGuesses) setGuesses(savedGuesses);
+
+        const savedMarkers = JSON.parse(await dataManager.get(`woordol_markers_${today}`));
+        if (savedMarkers) setMarkers(savedMarkers);
+
+        const savedData = JSON.parse(await dataManager.get(`woordol_data_${today}`));
+        if (savedData) {
+            if (savedData.round) {
+                setRound(savedData.round);
+            }
+        }
+
+        if (savedMarkers[round].every((guess) => guess === 'green')) setModalVisible(true);
     }
 
     const submit = () => {
@@ -54,7 +78,20 @@ export default function App() {
 
         // Check if all markers are green (= win)
         if (newMarkers[round].every((guess) => guess === 'green')) {
-            // TODO: make win system
+            const todaysDate = new Date().toISOString().slice(0, 10);
+            const todaysGuesses = JSON.stringify(guesses);
+            const todaysMarkers = JSON.stringify(newMarkers);
+            const todaysData = JSON.stringify({
+                day: todaysDate,
+                word: todaysWord,
+                round: round,
+            });
+
+            dataManager.store(`woordol_guesses_${todaysDate}`, todaysGuesses);
+            dataManager.store(`woordol_markers_${todaysDate}`, todaysMarkers);
+            dataManager.store('woordol_data', todaysData);
+
+            setModalVisible(true);
             setMarkers(newMarkers);
             return;
         }
@@ -86,6 +123,12 @@ export default function App() {
             });
 
             setLetterIndex(letterIndex - 1);
+
+            const todaysGuesses = JSON.stringify(guesses);
+            const todaysMarkers = JSON.stringify(markers);
+
+            dataManager.store('woordol_guesses', todaysGuesses);
+            dataManager.store('woordol_markers', todaysMarkers);
         }
     };
 
@@ -98,6 +141,12 @@ export default function App() {
             });
 
             setLetterIndex(letterIndex + 1);
+
+            const todaysGuesses = JSON.stringify(guesses);
+            const todaysMarkers = JSON.stringify(markers);
+
+            dataManager.store('woordol_guesses', todaysGuesses);
+            dataManager.store('woordol_markers', todaysMarkers);
         }
     };
 
@@ -115,6 +164,34 @@ export default function App() {
         } else if (pressed !== "enter") {
             // Enter new letter into the field
             enter(pressed);
+        }
+    };
+
+    const onShare = async () => {
+        try {
+            let shareMessage = '';
+            Object.keys(markers).forEach((v, i) => {
+                markers[i].forEach(m => {
+                    if (m === 'green') shareMessage += '🟩';
+                    else if (m === 'yellow') shareMessage += '🟨';
+                    else shareMessage += '⬛';
+                });
+                shareMessage += '\n';
+            });
+            const result = await Share.share({
+                message: shareMessage,
+            });
+            if (result.action === Share.sharedAction) {
+                if (result.activityType) {
+                    // shared with activity type of result.activityType
+                } else {
+                    // shared
+                }
+            } else if (result.action === Share.dismissedAction) {
+                // dismissed
+            }
+        } catch (error) {
+            alert(error.message);
         }
     };
 
@@ -164,6 +241,29 @@ export default function App() {
                 ))}
             </KeyboardContainer>
             <StatusBar style="auto" />
+            <ModalContainer>
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert("Modal has been closed.");
+                        setModalVisible(!modalVisible);
+                    }}
+                >
+                    <ModalContainer>
+                        <ModalView>
+                            <ModalText>Hello World!</ModalText>
+                            <View style={{ marginTop: 50 }}>
+                                <Button onPress={onShare} title="Share" />
+                            </View>
+                            <Pressable onPress={() => setModalVisible(!modalVisible)}>
+                                <ModalText>Hide Modal</ModalText>
+                            </Pressable>
+                        </ModalView>
+                    </ModalContainer>
+                </Modal>
+            </ModalContainer>
         </Container>
     );
 }
--- styles/home.js
+++ styles/home.js
@@ -4,3 +4,23 @@ export const Container = styled.View`
     flex: 1;
     background-color: #2d2e2c;
 `;
+
+export const ModalContainer = styled.View`
+    justify-content: center;
+    align-items: center;
+    margin-top: 22px;
+`;
+
+export const ModalView = styled.View`
+    margin: 20px;
+    background-color: white;
+    border-radius: 20px;
+    padding: 35px;
+    align-items: center;
+    elevation: 5;
+`;
+
+export const ModalText = styled.Text`
+    marginBottom: 15px;
+    text-align: center;
+`;
